44041d2e1300c7e9c975385cef75904a6b3ed682
Morwenn <morwenn29@hotmail.fr>
2025-11-05 22:25:25 +0100

    Resource for TSB005 and TSB006

--- amp/plot-amp.py
+++ amp/plot-amp.py
@@ -15,35 +15,34 @@ def compute_amp(sequence):
     ptp = max(shadow) - min(shadow)
     return len(sequence) - ptp - 1
 
-sequence = [4, 6, 6, 6, 7, 8, 6, 4, 3, 2, 5, 6, 1, 2, 7, 6, 5, 6, 8, 11, 12, 11, 9, 7, 8, 6]
+sequence = [4, 6, 6, 6, 7, 8, 6, 4, 3, 2, 5, 6, 1, 2, 7, 7, 5, 6, 8, 11, 11, 11, 9, 7, 8, 6]
 sequence.reverse()
-# sequence = [1, 2, 4, 5, 7, 8, 10, 13, 14, 15]
-# sequence = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10]
-# sequence = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2]
-# sequence = [1, 2, 3, 4, 5, 6, 1, 4, 2, 3]
+# sequence = [4, 6, 6, 6, 7, 8, 6, 4, 3, 2, 5, 6, 1, 2, 7, 6, 5, 6, 8, 11, 12, 11, 9, 7, 8, 6]  # TSB001
+# sequence = [1, 2, 4, 5, 7, 8, 10, 13, 14, 15]  # TSB001
+# sequence = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10]  # TSB001
+# sequence = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2]  # TSB001
+# sequence = [1, 2, 3, 4, 5, 6, 1, 4, 2, 3]  # TSB001
+
+
+
 diff = [0] + [comp(lhs, rhs) for lhs, rhs in itertools.pairwise(sequence)]
 shadow = numpy.cumsum(diff)
+neq = len([x for x in diff if x == 0]) - 1
 ptp = max(shadow) - min(shadow)
+amp = len(sequence) - ptp - neq - 1
 
 print("Sequence:", sequence)
-print("Order:", diff)
-print("Shadow:", shadow)
-print("PTP:", ptp)
-print("Amp:", len(sequence) - ptp - 1)
+print("Order(X):", diff)
+print("Shadow(X):", shadow)
+print("len(X):", len(sequence))
+print("Neq(X):", neq)
+print("PTP(X):", ptp)
+print("Amp(X):", amp)
 
 
 
 pyplot.plot(sequence, label="X", color="tab:blue")
-pyplot.plot(shadow, label="Original shadow(X)", color="tab:orange")
-
-shadow_range = list(range(len(shadow)))
-shadow_range.pop(10)
-shadow = list(shadow)
-shadow.pop(10)
-for i in range(10, len(shadow)):
-    shadow[i] -= 1
-pyplot.plot(shadow_range, shadow, label="Rectified shadow(X)", color="tab:green")
-pyplot.scatter([10, 10], [0, 6], marker="x", color="red", zorder=10)
+pyplot.plot(shadow, label="Shadow(X)", color="tab:orange")
 
 # pyplot.scatter([2, 3, 15, 20, 21, 2, 3, 15, 20, 21], [6, 6, 7, 11, 11, 1, 1, 2, 4, 4], marker="x", color="red", zorder=10)
 # pyplot.plot(
@@ -53,8 +52,6 @@ pyplot.scatter([10, 10], [0, 6], marker="x", color="red", zorder=10)
     # color="tab:green"
 # )
 
-
-
 graph_end = len(sequence) + 5
 
 pyplot.annotate("",
@@ -62,7 +59,7 @@ pyplot.annotate("",
     xytext=(graph_end, min(shadow)),
     arrowprops={
         "arrowstyle": "-",
-        "color": "green",
+        "color": "orange",
         "ls": "--",
     }
 )
@@ -71,7 +68,7 @@ pyplot.annotate("",
     xytext=(graph_end, max(shadow)),
     arrowprops={
         "arrowstyle": "-",
-        "color": "green",
+        "color": "orange",
         "ls": "--",
     }
 )
@@ -84,9 +81,9 @@ pyplot.annotate("",
         "ls": "--"
     }
 )
-pyplot.text(len(sequence), (min(shadow) + max(shadow)) / 2, "$PTP(X) = 6$")
+pyplot.text(len(sequence), (min(shadow) + max(shadow)) / 2, f"$PTP(X) = {ptp}$")
 
-pyplot.xlabel("Scenario 3: removing a $1$ from the pairwise order increases $PTP$")
+pyplot.xlabel(f"Random sequence, Amp(X) = {amp}")
 
 pyplot.xlim(xmin=0, xmax=graph_end)
 pyplot.legend()
